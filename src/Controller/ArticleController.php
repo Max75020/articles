ba9ad12db3a1ee1937e0aed8cba677c702e3c2c0
Maxime <maxime.duplaissy@gmail.com>
2023-11-23 11:48:21 +0100

    Ajout notification succés pour ajout article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,15 +13,21 @@ use App\Form\ArticleType;
 class ArticleController extends AbstractController
 {
 	#[Route('/', name: 'list_articles')]
-	public function index(ManagerRegistry $doctrine): Response
+	public function index(Request $request, ManagerRegistry $doctrine): Response
 	{
 
 		$em = $doctrine->getManager();
 
 		$articles = $em->getRepository(Article::class)->findAll();
 
+		$session = $request->getSession();
+		$notification = $session->get('notification');
+		$type_notif = $session->get('type_notif');
+
 		return $this->render('article/index.html.twig', [
 			'articles' => $articles,
+			'notification' => $notification,
+			'type_notif' => $type_notif
 		]);
 	}
 
@@ -39,7 +45,7 @@ class ArticleController extends AbstractController
 	}
 
 	#[Route('/view-article/{id_article}', name: 'view_article')]
-	public function viewArticle($id_article,ManagerRegistry $doctrine): Response
+	public function viewArticle($id_article, ManagerRegistry $doctrine): Response
 	{
 
 		$em = $doctrine->getManager();
@@ -52,7 +58,7 @@ class ArticleController extends AbstractController
 	}
 
 	#[Route('/article-add', name: 'add_article')]
-	public function addArticle(Request $request,ManagerRegistry $doctrine): Response
+	public function addArticle(Request $request, ManagerRegistry $doctrine): Response
 	{
 		$em = $doctrine->getManager();
 
@@ -60,14 +66,19 @@ class ArticleController extends AbstractController
 		$form = $this->createForm(ArticleType::class, $article);
 
 		$form->handleRequest($request);
-		
-		if($form->isSubmitted() && $form->isValid()){
+
+		if ($form->isSubmitted() && $form->isValid()) {
 			$article = $form->getData();
 
 			$article->setBody(nl2br($article->getBody()));
 
 			$em->persist($article);
 			$em->flush();
+
+			$session = $request->getSession();
+			$session->set('notification', "Article ajouté avec succès");
+			$session->set('type_notif', "alert-success");
+
 			return $this->redirectToRoute('list_articles');
 		}
 		return $this->render('article/addArticle.html.twig', [
